3caa12ced43614efae2411ad9d6595d2510cb12f
Danila <danila_moiseenkov@mail.ru>
2021-09-18 16:10:39 +0300

    Add parser arguments and SDTool pipeline

---
+++ SDTool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchDirectoryTool
+{
+    class SDTool
+    {
+        private Arguments arguments;
+        
+        public void pipeline(string[] args)
+        {
+            arguments = ArgumentParser.splitArguments(args);
+
+            
+        }
+    }
+}
