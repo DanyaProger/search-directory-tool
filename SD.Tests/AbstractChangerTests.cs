7027b252ec1b185696f52899c6a0a99fab030e24
Danila Maiseyenkau <danila_maiseyenkau@epam.com>
2024-03-30 00:22:46 +0300

    Create AbstractChangerTests class and implementation CmdTests class

---
+++ SD.Tests/AbstractChangerTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using WindowsInput;
+
+namespace SD.Tests
+{
+    internal abstract class AbstractChangerTests
+    {
+        private const string TEST_BIN_DIR = "test-bin";
+        private string oldPath;
+        private string sdDir;
+        private Process process;
+        protected InputSimulator inputSimulator = new InputSimulator();
+        protected abstract string GetChangerExe();
+        protected abstract int GetInitSleep();
+        protected abstract void Enter(string command);
+        protected abstract void EnterExit();
+
+
+        protected void BeforeAllTests()
+        {
+            DirectoryInfo directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            while (!directory.GetFiles("*.sln").Any())
+            {
+                directory = directory.Parent;
+            }
+
+            sdDir = Path.Combine(directory.FullName, TEST_BIN_DIR);
+            if (!Directory.Exists(sdDir))
+            {
+                throw new DirectoryNotFoundException();
+            }
+
+            File.WriteAllLines(Path.Combine(sdDir, "sd-pwd.bat"), new string[] { "@echo off", "echo %cd%> " + Path.Combine(sdDir, "pwd.txt") });
+
+            oldPath = Environment.GetEnvironmentVariable("PATH");
+            string newPath = sdDir + ";" + oldPath;
+            Environment.SetEnvironmentVariable("PATH", newPath);
+
+            process = new Process();
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.FileName = GetChangerExe();
+            process.StartInfo.CreateNoWindow = false;
+
+            process.Start();
+
+            Thread.Sleep(GetInitSleep());
+        }
+
+        protected void TestSdHome()
+        {
+            Enter("sd home");
+
+            Thread.Sleep(1000);
+
+            Enter("sd-pwd.bat");
+
+            Thread.Sleep(500);
+
+            string actualDir = File.ReadAllLines(Path.Combine(sdDir, "pwd.txt"))[0];
+            Assert.AreEqual("C:\\Users\\Danila_Maiseyenkau\\work", actualDir);
+        }
+
+        protected void AfterAllTests()
+        {
+            EnterExit();
+
+            process.WaitForExit();
+
+            Environment.SetEnvironmentVariable("PATH", oldPath);
+        }
+    }
+}
---
+++ SD.Tests/cmd/CmdTests.cs
@@ -0,0 +1,47 @@
+﻿using WindowsInput;
+
+namespace SD.Tests.cmd
+{
+    internal class CmdTests : AbstractChangerTests
+    {
+        protected override string GetChangerExe()
+        {
+            return "cmd.exe";
+        }
+        protected override int GetInitSleep()
+        {
+            return 2000;
+        }
+
+        protected override void Enter(string command)
+        {
+            inputSimulator.Keyboard.TextEntry(command);
+            inputSimulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+        }
+
+        protected override void EnterExit()
+        {
+            inputSimulator.Keyboard.TextEntry("exit");
+            inputSimulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+        }
+
+        [OneTimeSetUp]
+        public new void BeforeAllTests()
+        {
+            base.BeforeAllTests();
+        }
+
+        [OneTimeTearDown]
+        public new void AfterAllTests()
+        {
+            base.AfterAllTests();
+        }
+
+
+        [Test]
+        public new void TestSdHome()
+        {
+            base.TestSdHome();
+        }
+    }
+}
